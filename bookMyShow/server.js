6d82f5167286d66b28a8873b687306ae22e5010b
reemaroy <reemaroy024@gmail.com>
2019-07-01 09:15:54 +0530

    node and database added

---
+++ bookMyShow/server.js
@@ -0,0 +1,60 @@
+var hapi=require('@hapi/hapi');
+require("dotenv").config();
+var mysql=require('mysql');
+
+var server=new hapi.Server({
+    host:'localhost',
+    port:4180,
+    routes : {
+            cors : true
+        }
+});
+
+
+server.route({
+    method:"GET",
+    path:"/api/movies",
+    handler:(request,reply)=>{
+      return new Promise((resolve,reject)=>{
+            var connection = mysql.createConnection({
+                host     : process.env.DB_HOST,
+                user     : process.env.DB_USER,
+                password : process.env.DB_PASSWORD,
+                database : process.env.DB_NAME
+              });
+              connection.connect();
+              connection.query(`SELECT * from movies`, function (error,movies, fields) {
+                if (error) reject(error);
+                resolve(movies);
+              }); 
+              connection.end();
+        })
+      } 
+});
+
+server.route({
+    method:"POST",
+    path:"/api/theatres",
+    handler:(request,reply)=>{
+    	var id=request.payload;
+      return new Promise((resolve,reject)=>{
+            var connection = mysql.createConnection({
+                host     : process.env.DB_HOST,
+                user     : process.env.DB_USER,
+                password : process.env.DB_PASSWORD,
+                database : process.env.DB_NAME
+              });
+              connection.connect();
+              connection.query(`SELECT * from theatres where mid='${id}'`, function (error,theatres, fields) {
+                if (error) reject(error);
+                resolve(theatres);
+              });  
+              connection.end();
+        })   
+    }
+});
+server.start((err)=>{
+    if(err) throw err;
+    
+})
+console.log("Server is started"+ server.info.uri)
